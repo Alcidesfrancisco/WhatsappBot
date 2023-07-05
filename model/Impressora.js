b857bea9795556999b8315e34196ed6e4c83469e
Alcides Francisco <alcidesfrancisco@gmail.com>
2023-07-05 06:00:21 -0300

    mudei o nome da pasta pojo para model

---
+++ model/Impressora.js
@@ -0,0 +1,13 @@
+class Impressora{
+    constructor(marca, modelo, serial, ip, nivel_toner, paginas){
+        this.marca = marca;    
+        this.modelo = modelo;
+        this.serial = serial;
+        this.ip = ip;
+        this.nivel_toner = nivel_toner;
+        this.paginas = paginas;
+        }
+
+
+}
+export {Impressora}
---
+++ model/Mensagem.js
@@ -0,0 +1,48 @@
+import {Impressora} from './Impressora.js';
+class Mensagem{
+    constructor(){
+        this.texto = "";
+        this.impressora = null;
+        this.status = 'NAO_LIDA';
+    }
+    get_numero(){ return this.numero;}
+    get_texto(){ return this.texto}
+    set_texto(texto){this.texto = texto}
+    get_impressora(){ return this.impressora}
+    get_status(){ return this.status}
+    set_status(status){this.status = status};
+
+    compor_mensagem(mensagem_zap){
+        try{
+        let linhas = mensagem_zap.split("\n");
+        this.texto = linhas.shift();
+
+        var impressora = new Impressora();
+        impressora.marca = linhas.shift().split(' ')[1];
+        impressora.modelo = linhas.shift().split(' ')[1];
+        impressora.serial = linhas.shift().split(' ')[1];;
+        impressora.ip = linhas.shift().split(' ')[1];
+        impressora.nivel_toner = linhas.shift().split(' ')[3];
+        linhas.shift();
+        impressora.paginas = linhas.shift().split(' ')[2];
+        this.impressora = impressora;
+        this.status = "NAO_LIDA";
+        }catch(erro){
+            console.log(erro);
+        }
+        return this;
+    }
+
+    comparar_mensagem(obj_mensagem) {
+        if((obj_mensagem.impressora.status === this.impressora.status)) {
+           console.log('mensagens iguais, não adiciona') ;
+           return false;
+        }else if((obj_mensagem.impressora.status !== this.impressora.status)) {
+            console.Console.log('mensagens diferentes, adicione a lista') ;
+            return true;
+        }
+    }
+
+
+}
+export {Mensagem};
